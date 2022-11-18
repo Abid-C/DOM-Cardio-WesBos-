fbc4d80977e945682d630382dcd6dae13866d13d
Abid-C <abidchowdhury56@gmail.com>
2022-11-18 08:31:34 -0500

    created and appended a div with the class - wrapper using javascript

---
+++ DOM-Cardio.js
@@ -0,0 +1,8 @@
+const div_with_wrapper = document.createElement('div');
+div_with_wrapper.classList.add('wrapper');
+document.body.appendChild(div_with_wrapper);
+
+const unordered_list = document.createElement('ul');
+unordered_list.textContent = 'DO
+
+div_with_wrapper.appendChild(unordered_list);
