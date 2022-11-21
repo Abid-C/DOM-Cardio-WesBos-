59b96a1c3b62592d2c512cd4f230f389c1e3e872
Abid-C <abidchowdhury56@gmail.com>
2022-11-21 09:40:53 -0500

    Created an unordered list inside the white area

--- DOM-Cardio.js
+++ DOM-Cardio.js
@@ -1,8 +1,35 @@
+//DOM title with the class wrapper
 const div_with_wrapper = document.createElement('div');
 div_with_wrapper.classList.add('wrapper');
 document.body.appendChild(div_with_wrapper);
 
+//List heading
 const unordered_list = document.createElement('ul');
-unordered_list.textContent = 'DO
+unordered_list.textContent = 'DOM-Cardio';
+unordered_list.classList.add('list_header');
 
 div_with_wrapper.appendChild(unordered_list);
+
+//const stylise_the_header = `
+//     <style>
+//         .list_header {
+//             color: blue;
+//         }
+//     </style>
+// `;
+
+// const body = document.querySelector('body');
+
+// body.insertAdjacentElement('afterend', stylise_the_header);
+
+//Creating list items
+const li_one = document.createElement('li');
+li_one.textContent = 'One';
+const li_two = document.createElement('li');
+li_two.textContent = 'Two';
+const li_three = document.createElement('li');
+li_three.textContent = 'Three';
+
+unordered_list.appendChild(li_two);
+li_two.insertAdjacentElement('beforebegin', li_one);
+li_two.insertAdjacentElement('afterend', li_three);
